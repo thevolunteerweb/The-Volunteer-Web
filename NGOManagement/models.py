336f14182411e6fe120284a0278369e65c8e0a6f
Tejas <tejas.ramdas@gmail.com>
2016-06-18 23:00:11 +0530

    DateField fix

--- NGOManagement/models.py
+++ NGOManagement/models.py
@@ -2,10 +2,10 @@ from django.db import models
 import datetime
 
 class Offline_Vol(models.Model):
-    date_time_req = models.DateTimeField(auto_now_add=True)
+    date_time_req = models.DateTimeField(auto_now_add=True,null=True)
     ngo_id = models.TextField(max_length = 100, blank = False)
     volunteer_name = models.TextField(max_length = 100, blank = True)
-    date_vol = models.DateField(blank=True)
+    date_vol = models.DateField(,null=True,blank=True)
     time_vol = models.TimeField(blank=True)
     hours_vol = models.PositiveSmallIntegerField()
     activity = models.TextField(max_length = 100, blank = False)
@@ -16,7 +16,7 @@ class Offline_Donations(models.Model):
     amount_donated = models.PositiveIntegerField()
     cause = models.TextField(max_length = 1000, blank = True)
     mode_of_payment = models.TextField(default = "Not Specified", max_length = 30)
-    date_donated = models.DateField(blank=True)
+    date_donated = models.DateField(blank=True,null=True)
 
 class Expenditure (models.Model):
     ngo_id = models.TextField(max_length = 100, blank = False)
