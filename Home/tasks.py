f868e086f3bf08ea304da75213b408f57afe0a38
Tejas <tejas.ramdas@gmail.com>
2016-06-18 17:14:37 +0530

    Tasks experimentation

--- Home/tasks.py
+++ Home/tasks.py
@@ -4,7 +4,7 @@ from Requests.models import Volunteer_ngo_request
 import datetime
 @task
 def duplicate(a,b,c,d,e):
-    Volunteer_ngo_request.objects.create(sender = a, recepient = b, date_vol = c, time_vol = d, activity = e, onetime = False, status = "Accept")
+    Volunteer_ngo_request.objects.create(sender = a, recepient = b, status = "Accept")
     return "Success"
 @task
 def sum(a,b):
