672cb5cbe8c35bdab6587aae906eba978397695b
Tejas <tejas.ramdas@gmail.com>
2016-06-18 19:00:12 +0530

    Testing celery

--- Registration/views.py
+++ Registration/views.py
@@ -135,8 +135,6 @@ def update_ngoprofile(request):
         else:
             return HttpResponse("Invalid form")
     else:
-        a=duplicate.apply_async(countdown=10,args=["CELERYASYNC"])
-        print(a)
         try:
             NGOProfile.objects.get(ngo_id=request.user.id)
 
