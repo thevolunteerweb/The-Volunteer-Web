85f07bb117f3ab01869f30c4cebb11f3a874af65
Tejas <tejas.ramdas@gmail.com>
2016-06-18 19:25:10 +0530

    POSTGRESQL

--- YouTeach/settings.py
+++ YouTeach/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -103,12 +104,7 @@ WSGI_APPLICATION = 'YouTeach.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES['default'] =  dj_database_url.config()
 LOGIN_REDIRECT_URL='/'
 LOGOUT_REDIRECT_URL='/'
 # Internationalization
